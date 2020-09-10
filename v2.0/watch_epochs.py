71ec4205d29a6bb816a2859a23fcba76b680eb2d
zcc <listenzcc@mail.bnu.edu.cn>
2020-09-10 12:21:33 +0800

    Daily update

--- v2.0/watch_epochs.py
+++ v2.0/watch_epochs.py
@@ -8,3 +8,18 @@ dm = DataManager(name)
 dm.load_epochs(recompute=False)
 
 # %%
+dm.epochs_list[0]
+# %%
+dm.epochs_list[2]['4'].average().plot_joint()
+# %%
+dm.epochs_list
+# %%
+epochs_1, epochs_2 = dm.leave_one_session_out(includes=[1, 3, 5],
+                                              excludes=[2, 4, 6])
+
+# %%
+for e in ['1', '2', '3', '4']:
+    epochs_1[e].average().plot_joint(title=e)
+print()
+
+# %%
